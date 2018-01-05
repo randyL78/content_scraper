73b61b01ef1248773a4465aa2702dd746aed5aa6
RandyL <30959856+randyL78@users.noreply.github.com>
2018-01-05 18:15:10 -0500

    created array complete event

--- scraper.js
+++ scraper.js
@@ -6,13 +6,14 @@ const fs = require('fs');
 const scrapeIt = require("scrape-it")
 // transpiler to convert json data to a csv file
 const j2c = require('json2csv');
+// my way around the async problem
+const events = require('events');
+const emitter = new events.EventEmitter();
 
 // ==================== Configuration and global variables
 const baseURL = "http://shirts4mike.com/";
 const time = new Date();
-
-
-
+let csv=[];
 
 // ==================== Scrape main site for shirt urls
 scrapeIt(baseURL + "shirts.php", {
@@ -23,38 +24,17 @@ scrapeIt(baseURL + "shirts.php", {
         data: {
             url: {
                 selector: "a",
-                attr: "href"
+                attr: "href",
             }
         }
     }
-})
-
-.then(data => {
-// ==================== Prepare the directory for incoming files
-    // Create a 'data' folder if none exists
-
-    if (!fs.existsSync("data/")) {
-        fs.mkdir("data/", (err) => {
-            if (err) throw new Error("Problem creating directory");
-            console.log("succesfully created data folder");
-        });
-    } else {
-        console.log(`"data" folder already exists, leaving intact`);
-    }
-    return data;
-})
-
-.then(data => {
-
-
-
 })
 // ==================== Get shirt data
 // cycle through each scraped url and fetch data for each shirt
 .then(data => {
     if(!data) throw new Error(`No urls recovered, please check your internet connection or ${baseURL} may be down`);
     data.pages.forEach(page => {
-        scrapeIt(baseURL + page.url, { 
+       scrapeIt(baseURL + page.url, { 
             Title: {
                 selector: "img",
                 attr    : "alt"
@@ -64,19 +44,43 @@ scrapeIt(baseURL + "shirts.php", {
                 selector: "img",
                 attr    : "src"
             }
-        }).then(data => {
-            data.URL    = baseURL + page.url;
-            data.Time   = new Date();
-            shirtInfo.push(data);
-            console.log(data);
+        }).then(shirt => {
+            shirt.URL    = baseURL + page.url;
+            shirt.Time   = new Date();
+           
+            csv.push(shirt);
+            if (csv.length === data.pages.length) {
+                emitter.emit("complete");
+            }
+        }).catch( err => {
+            console.error(err.message);
         })
-        
+    })
+})
+.then( () => {
+// ==================== Prepare the directory for incoming files
+    // Create a 'data' folder if none exists
+    if (!fs.existsSync("data/")) {
+        fs.mkdir("data/", (err) => {
+            if (err) throw new Error("Problem creating directory");
+            console.log("succesfully created data folder");
+        });
+    } else {
+        console.log(`"data" folder already exists, leaving intact`);
+    }
+}).then( () => {
+// ==================== Create an event handler for when last item is pushed to array
+    emitter.on("complete", () => {
+
+
+
     });
 })
 
+// =================== Handle errors
 
 .catch( err => {
-    console.error("An error occured, please see file scraper-error.log for more details");
+    console.error(err.message);
 })
 
 // ==================== Convert Data stream to json
