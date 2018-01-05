64b58117b4b926c455aa5c13a5f0812a1fcbc7b1
RandyL <30959856+randyL78@users.noreply.github.com>
2018-01-05 14:44:05 -0500

    Added individual shirt scraping

--- scraper.js
+++ scraper.js
@@ -9,13 +9,12 @@ const j2c = require('json2csv');
 
 // ==================== Configuration and global variables
 const baseURL = "http://shirts4mike.com/";
+const time = new Date();
 
 
 
 
-// ==================== Scrape the website
- 
-// scrape main page for shirt urls
+// ==================== Scrape main site for shirt urls
 scrapeIt(baseURL + "shirts.php", {
     // Fetch the links to individual pages
     pages: {
@@ -29,35 +28,66 @@ scrapeIt(baseURL + "shirts.php", {
         }
     }
 })
+
 .then(data => {
-    // data
+// ==================== Prepare the directory for incoming files
+    // Create a 'data' folder if none exists
+
+    if (!fs.existsSync("data/")) {
+        fs.mkdir("data/", (err) => {
+            if (err) throw new Error("Problem creating directory");
+            console.log("succesfully created data folder");
+        });
+    } else {
+        console.log(`"data" folder already exists, leaving intact`);
+    }
+    return data;
+})
+
+.then(data => {
+
+
+
+})
+// ==================== Get shirt data
+// cycle through each scraped url and fetch data for each shirt
+.then(data => {
+    if(!data) throw new Error(`No urls recovered, please check your internet connection or ${baseURL} may be down`);
     data.pages.forEach(page => {
-        console.log(page.url);
+        scrapeIt(baseURL + page.url, { 
+            Title: {
+                selector: "img",
+                attr    : "alt"
+            },
+            Price: ".price",
+            ImageURL: { 
+                selector: "img",
+                attr    : "src"
+            }
+        }).then(data => {
+            data.URL    = baseURL + page.url;
+            data.Time   = new Date();
+            shirtInfo.push(data);
+            console.log(data);
+        })
+        
     });
+})
 
 
-}, err => {
-    console.error(`There was a problem reaching ${entryURL}`);
+.catch( err => {
+    console.error("An error occured, please see file scraper-error.log for more details");
 })
 
 // ==================== Convert Data stream to json
 
-// ==================== Prepare the directory for incoming files
-// Create a 'data' folder if none exists
-if (!fs.existsSync("data/")) {
-    fs.mkdir("data/", (err) => {
-        if (err) throw err;
-        console.log("succesfully created data folder");
-    });
-} else {
-    console.log(`"data" folder already exists, leaving intact`);
-}
 
 
 
 
 
-// ==================== Error handling
+
+
 
 
 
